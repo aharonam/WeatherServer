28ed73e458efe6f014da8c0c9713bf08104fe862
Aharon Amram <aharonam@gmail.com>
2016-02-11 17:04:57 +0200

    add weather

---
+++ routes/weater.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Aharon on 2/11/2016.
+ */
+var express = require('express');
+var router = express.Router();
+var url = require('url');
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    let url_parts = url.parse(req.url, true);
+    var text = url_parts.text;
+    res.send(text);
+});
+
+module.exports = router;
