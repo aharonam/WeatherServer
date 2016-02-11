c09c2abd6b73ec50be40eded4208af7462444bb9
Aharon Amram <aharonam@gmail.com>
2016-02-11 17:21:48 +0200

    add weather

--- routes/weather.js
+++ routes/weather.js
@@ -3,13 +3,11 @@
  */
 var express = require('express');
 var router = express.Router();
-var url = require('url');
 
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    var url_parts = url.parse(req.url, true);
-    var text = url_parts.text;
+    var text = req.param('text');
     res.send(text);
 });
 
